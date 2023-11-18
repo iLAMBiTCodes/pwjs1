99b7b583a5f2263e7aa9792875b0fcf4c72d3481
iLAMBiTCodes <ilambitexting@gmail.com>
2023-11-18 10:19:01 +0530

    added lines

--- basics1/2.variables-constants.js
+++ basics1/2.variables-constants.js
@@ -9,3 +9,10 @@ const age = 18;
 let user_password = "Lambit@123"
 const password = "123"
 console.log(password)
+
+// Variable names must start with a letter, an underscore (_) or a dollar sign ($).
+// Variable names cannot contain spaces.
+// Variables cannot be the same as reserved keywords such as if or const.
+// By convention, JavaScript variable names are written in camelCase.
+// Variables should be given descriptive names that indicate their content and usage (e.g. sellingPrice and costPrice rather than x and y).
+// As JavaScript variables do not have set types, it can be useful to include an indication of the type in the name (e.g. orderNumber is obviously a numeric ID, whereas order could be an object, a string or anything else).
